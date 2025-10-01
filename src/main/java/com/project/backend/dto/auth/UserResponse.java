62f3deb43d6d14358b1cc2918c4f3ff52c7d182f
QuyDang <dangquyy1108@gmail.com>
2025-10-01 12:29:39 +0700

    add lombok

--- src/main/java/com/project/backend/dto/auth/UserResponse.java
+++ src/main/java/com/project/backend/dto/auth/UserResponse.java
@@ -3,7 +3,9 @@ package com.project.backend.dto.auth;
 import com.project.backend.entity.User;
 import java.time.Instant;
 import java.util.Date;
+import lombok.Data;
 
+@Data
 public class UserResponse {
     private Long id;
     private String username;
@@ -15,87 +17,6 @@ public class UserResponse {
     private Instant lastLogin;
     private String role;
     private String status;
-
-    // Getters & Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Integer getStreak() {
-        return streak;
-    }
-
-    public void setStreak(Integer streak) {
-        this.streak = streak;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public Instant getLastLogin() {
-        return lastLogin;
-    }
-
-    public void setLastLogin(Instant lastLogin) {
-        this.lastLogin = lastLogin;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 
     public static UserResponse fromEntity(User user) {
         UserResponse userResponse = new UserResponse();
