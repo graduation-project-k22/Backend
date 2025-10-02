ddcec4d4adbb17e13237839d471d18d8f78aaddd
QuyDang <dangquyy1108@gmail.com>
2025-10-02 19:48:57 +0700

    config

--- src/main/java/com/project/backend/configs/CustomAccessDeniedHandler.java
+++ src/main/java/com/project/backend/configs/CustomAccessDeniedHandler.java
@@ -3,17 +3,19 @@ package com.project.backend.configs;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
-    public void handle(HttpServletRequest request,
+    public void handle(HttpServletRequest
+                               request,
                        HttpServletResponse response,
                        AccessDeniedException accessDeniedException) throws IOException {
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
